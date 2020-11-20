cf6565642745a66b36a85c435af7de1d3bf4a8f2
demetriodeis <demetriodeis@gmail.com>
2020-11-20 11:47:09 -0300

    EF Core construction and migration.

---
+++ CodersAcademy/CodersAcademy.API/Respository/AlbumRepository.cs
@@ -0,0 +1,20 @@
+﻿using CodersAcademy.API.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Respository
+{
+    public class AlbumRepository
+    {
+        private MusicContext Context { get; init; }
+        public AlbumRepository(MusicContext context)
+        {
+            this.Context = context;
+        }
+        public async Task<IList<Album>> GetAllAsync()
+            => await this.Context.Albums.ToListAsync();
+    }
+}
---
+++ CodersAcademy/CodersAcademy.API/Respository/MusicContext.cs
@@ -0,0 +1,26 @@
+﻿using CodersAcademy.API.Model;
+using CodersAcademy.API.Respository.Mapping;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodersAcademy.API.Respository
+{
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+        public MusicContext(DbContextOptions<MusicContext> options): base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new AlbumMapping());
+            modelBuilder.ApplyConfiguration(new MusicMapping());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
